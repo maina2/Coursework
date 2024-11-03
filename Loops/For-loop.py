2f4353c0bb0948c5d32b6aff4cf0c39a6666e83f
Simon Chanzu <chanzusimon6@gmail.com>
2024-11-03 18:55:45 +0300

    uploads today

---
+++ Loops/For-loop.py
@@ -0,0 +1,31 @@
+# numbers = [10, 20, 30, 40, 50]
+# total = 0
+
+# for numero in numbers:
+#     total = total+ numero
+# print("Total sum:", total)
+
+# num = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num * i}")
+
+
+# n = int(input("Enter a positive integer: "))
+# total = 0
+# for i in range(1, n + 1):
+#     total += i
+# print("Sum of the first", n, "natural numbers is:", total)
+
+# Count Vowels in a String
+text = str(input("Enter the desired string: "))
+vowels= "aeiouAEIOU"
+count= 0
+
+for vowel in text :
+    if vowel in vowels :
+        count+= 1
+        print("the number of vowels are:: \n ",  vowel)
+print("the number of vowels is::",count )
+
+
+
