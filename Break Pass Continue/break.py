b0f97ebbaae711846d29c30f5aac1cf337797d86
Simon Chanzu <chanzusimon6@gmail.com>
2024-11-05 10:23:48 +0300

    uploads

---
+++ Break Pass Continue/break.py
@@ -0,0 +1,29 @@
+
+# # Databricks program
+# vacancies = 5
+
+# applicants = int(input("Please enter your application number: "))
+
+# i = 0
+
+# while i<=applicants:
+#     if i>=vacancies:
+#         print("We are out of positions")
+#         break
+
+#     print("You have applied for the enrolment")
+#     i+=1
+
+
+# print("Finished the application process")
+
+
+
+
+for i in range(1,101):
+   
+    if i%3 == 0 :
+        continue
+
+    print(i)
+    
---
+++ Break Pass Continue/prime_number.py
@@ -0,0 +1,5 @@
+
+for i in range(1,10):
+    if i % i == 0 and i%1 == 0:
+        print(i)
+        
